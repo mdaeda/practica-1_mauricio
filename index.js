17c0ee681e2f03a449413b32d25a358c2e56093c
mdaeda <89814200+mdaeda@users.noreply.github.com>
2021-09-07 17:47:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ index.js
 
 fuction getIMC(){
   ver peso=parsefloat(document.
-                      get element ById(¨peso¨).
+                      get element ById(¨peso¨).value;
+  ver altura=parsefloat(document.
+                        get element ById(¨altura¨).value);
+  ver IMC= peso /(altura*altura);
+  document=getElement ById (¨IMC¨).inner HTML=imc;
+}
